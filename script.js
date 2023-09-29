ca3c46de7784bb82eddac184e7d0107ade5d1707
Ani <131550720+ani-menaphire@users.noreply.github.com>
2023-09-29 19:44:14 +0400

    Write createGrid function

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector('#container');
+
+function createGrid(columnNum) {
+  container.classList.add('frame')
+  for (x = 1; x <= columnNum; x++) {
+    const column = document.createElement('div')
+    column.setAttribute('id', 'column')
+    container.append(column)
+  
+    for (y = 1; y <= columnNum; y++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', 'square')
+      column.append(square)
+    }
+  }  
+}
+
+createGrid(16)
