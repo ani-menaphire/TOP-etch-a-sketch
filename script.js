12493cede03b91763c4fa600cc8b4b58c9e31ff8
Ani <131550720+ani-menaphire@users.noreply.github.com>
2024-04-19 19:24:06 +0400

    Make Changes

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
+'use strict'
+
 const gridBtn = document.querySelector('#change-canvas');
 const clearBtn = document.querySelector('#clear-canvas')
 const container = document.querySelector('#container');
 const colorRandomizerBtn = document.querySelector('#randomize-color');
 const blackBtn = document.querySelector('#black');
 const gradualBtn = document.querySelector('#gradual')
-
-let changeGrid
-
+let squares
+let recentFunction 
 function createGrid(columnNum) {
   container.classList.add('frame')
-  for (x = 1; x <= columnNum; x++) {
+  for (let x = 1; x <= columnNum; x++) {
     const column = document.createElement('div');
     column.classList.add('column');
     container.append(column);
   
-    for (y = 1; y <= columnNum; y++) {
+    for (let y = 1; y <= columnNum; y++) {
       const square = document.createElement('div');
       square.classList.add('square');
       column.append(square);
@@ -23,132 +24,92 @@ function createGrid(columnNum) {
       })*/
     }
   }  
-  begin()
+  //begin()
+  squares = document.querySelectorAll('.square');
+  squares.forEach(square => {
+    square.addEventListener('mouseover', recentFunction)
+  })
 }
 
-let col = 16;
-createGrid(col)
-let randomColor
+
+createGrid(16)
+
 function getRandomColor() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
-  randomColor = `rgb(${red}, ${green}, ${blue})`
-  return randomColor;
+  return `rgb(${red}, ${green}, ${blue})`
 }
-
-
-function begin() {
-  const squares = document.querySelectorAll('.square');
-  squares.forEach(square => {
-    square.style.backgroundColor = 'white';
-  })
-  let lastUsedFunction 
+ 
   
-  clearBtn.addEventListener('click', () => {
-  /*const columns = document.querySelectorAll('.column');
-  columns.forEach(column => {
-    column.remove(); 
-  }) 
-  createGrid(col)*/
-
+clearBtn.addEventListener('click', () => {
   squares.forEach(square => {
-    //square.removeEventListener('mouseover', lastUsedFunction)
     square.style.backgroundColor = 'white'
-    
   })
 }) 
 
-function changeColor() {
+function paintColor() {
   this.style.backgroundColor = getRandomColor()
 }
 
-function changeBlack() {
+function paintBlack() {
   this.style.backgroundColor = 'black';
 }
 
 function colorRainbow() {
   squares.forEach(square => {
-    square.removeEventListener('mouseover', lastUsedFunction)
-    square.addEventListener('mouseover', changeColor)
+    square.removeEventListener('mouseover', recentFunction)
+    square.addEventListener('mouseover', paintColor)
   })
-  lastUsedFunction = changeColor
+  recentFunction = paintColor
 }
 
 function colorBlack() {
-  squares.forEach(square => {
-    square.removeEventListener('mouseover', lastUsedFunction)
-    square.addEventListener('mouseover', changeBlack)
-  })
-  lastUsedFunction = changeBlack
-}
-
-function colorGradual() {
   
   squares.forEach(square => {
-    let opacity
-    square.removeEventListener('mouseover', lastUsedFunction)
-    square.addEventListener('mouseover', ()=> {
-      console.log(square.style.backgroundColor)
-      if (square.style.backgroundColor == `rgba(0, 0, 0, 0.1)` ||
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.2)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.3)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.4)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.5)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.6)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.7)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.8)` || 
-          square.style.backgroundColor == `rgba(0, 0, 0, 0.898)` || 
-          square.style.backgroundColor == `rgb(0, 0, 0)`   
-      ) {
-        
-      }
-      else opacity = 0;
-      if (square.style.backgroundColor == `rgba(0, 0, 0)` || opacity >= 1) return;
-      else square.style.backgroundColor = `rgba(0, 0, 0, ${opacity += 0.1})`;
-    })
+    square.removeEventListener('mouseover', recentFunction)
+    square.addEventListener('mouseover', paintBlack)
   })
+  recentFunction = paintBlack
 }
 
-colorRandomizerBtn.addEventListener('click', colorRainbow);
-
-blackBtn.removeEventListener('click', colorBlack);
-blackBtn.addEventListener('click', colorBlack);
 
 
-gradualBtn.addEventListener('click', colorGradual)
 
-/*function resetGrid(number) {
-  if (number === null) {
-    return;
+function colorGradual() {
+  squares.forEach(square => {
+    let opacity
+    square.removeEventListener('mouseover', recentFunction)
+    square.addEventListener('mouseover', function paintGradual() {
+      if (!this.style.backgroundColor.includes(`rgba(0, 0, 0`) &&
+          this.style.backgroundColor !== 'rgb(0, 0, 0)') {
+        opacity = 0;
+      }
+      this.style.backgroundColor = `rgba(0, 0, 0, ${opacity += 0.1})`;
+    }) 
+    
+  })
+  //recentFunction = paintGradual
+  console.log(recentFunction) 
+  recentFunction = colorGradual
+}
+
+function changeGrid()  {
+  let col = prompt('Enter a number up to 100 to generate a grid with that many columns and rows');
+  if (col === null) return;
+  else if (col > 100) {
+    col = prompt('I\'m afraid that\'s more than 100');
   }
-  else if (number > 100) {
-    number = prompt('I\'m afraid that\'s more than 100');
-    resetGrid(number);
-  }
-  columns = document.querySelectorAll('#column')
+  const columns = document.querySelectorAll('.column')
   columns.forEach(column => {
     column.remove()
   }); 
-  createGrid(number)
-} */
 
-  changeGrid = () => {
-    col = prompt('Enter a number up to 100 to generate a grid with that many columns and rows');
-    if (col === null) return;
-    else if (col > 100) {
-      col = prompt('I\'m afraid that\'s more than 100');
-      resetGrid()
-    }
-    const columns = document.querySelectorAll('.column')
-    columns.forEach(column => {
-      column.remove()
-    }); 
-    gradualBtn.removeEventListener('click', colorGradual)
-    gridBtn.removeEventListener('click', changeGrid)
-    createGrid(col)
-  } 
+  createGrid(col)
+
+} 
   
-  gridBtn.addEventListener('click', changeGrid)
-}
-
+gridBtn.addEventListener('click', changeGrid);
+colorRandomizerBtn.addEventListener('click', colorRainbow);
+blackBtn.addEventListener('click', colorBlack);
+gradualBtn.addEventListener('click', colorGradual);
